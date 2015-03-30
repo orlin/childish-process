0b92d8005495eb3b0c326db292c296a336ef4a8b
Orlin M Bozhinov <om@soundsapiens.com>
2015-03-30 21:42:55 +0300

    make use of beverage

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,8 @@
-var gulp = require('gulp')
-var gutil = require('gulp-util')
-var coffee = require('gulp-coffee')
-var sourcemaps = require('gulp-sourcemaps')
-
-gulp.task('coffee', function() {
-  gulp.src('./*.coffee')
-    .pipe(sourcemaps.init())
-    .pipe(coffee({bare: true}).on('error', gutil.log))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./'))
+var gulp = require('beverage')(require('gulp'), {
+  scripts: {
+    include: {'build': 'Compile coffee with inline source-map.'}
+  },
+  buildWatch: ['index.coffee']
 })
 
-gulp.task('watch', function(){
-  gulp.watch('./*.coffee', ['coffee'])
-})
-
-gulp.task('default', ['coffee', 'watch'])
+gulp.task('dev', 'DEVELOP', ['build', 'build:watch'])
